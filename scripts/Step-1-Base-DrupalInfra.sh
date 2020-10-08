b0274ea55ae848ed12e700245023adbbd08779b7
Walter Myers III <walterm@microsoft.com>
2020-10-08 08:49:29 -0700

    Create Step-1-Base-DrupalInfra.sh

---
+++ scripts/Step-1-Base-DrupalInfra.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#######################
+### RUN THIS FIRST  ###
+#######################
+
+rg="DrupalInfra"
+primaryRegion="eastus2"
+secondaryRegion="centralus"
+spokeVNet="spokeVNet"
+spokeVNetAddressPrefix="10.0.0.0/16"
+spokeAppSubnet="spokeAppSubnet"
+spokeAppSubnetAddressPrefix="10.0.1.0/24"
+spokeDBSubnet="spokeDBSubnet"
+spokeDBSubnetAddressPrefix="10.0.2.0/24"
+scaleset="drupalScaleSet"
+scalesetLB="drupalScaleSetLB"
+instanceCount="2"
+adminUsername="drupaladmin"
+adminPassword="tst909@@10"
+privEndpointConnection="mysqlprivendconn"
+
+# MySQL related parameters 
+mysqlprimary="drupalmysqlprimary"
+mysqlreplica1="drupalmysqlreplica1"
+
+# Create a resource group.
+echo "Creating Resource Group"
+az group create --name $rg --location $primaryRegion
+
+# Create VNet
+echo "Creating Virtual Network"
+az network vnet create \
+  --name $spokeVNet \
+  --resource-group $rg \
+  --address-prefix $spokeVNetAddressPrefix \
+  --subnet-name $spokeAppSubnet \
+  --subnet-prefix $spokeAppSubnetAddressPrefix
+
+echo "Add Service Endpoint to App Subnet"
+az network vnet subnet update  \
+  --resource-group $rg \
+  --name $spokeAppSubnet \
+  --vnet-name $spokeVNet \
+  --service-endpoints Microsoft.SQL
+
+echo "Add DB Subnet"
+az network vnet subnet create \
+  --name $spokeDBSubnet \
+  --resource-group $rg \
+  --vnet-name $spokeVNet \
+  --address-prefix $spokeDBSubnetAddressPrefix
